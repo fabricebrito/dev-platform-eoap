f12db9f2c87d83812b133e26c370411f4de33f6b
Fabrice Brito <fabrice.brito@terradue.com>
2024-10-14 14:11:17 +0200

    adds ogc zoo project

---
+++ ogc-api-processes-with-zoo/files/init.sh
@@ -0,0 +1,31 @@
+cd /workspace
+
+    git clone https://github.com/eoap/ogc-api-processes-with-zoo.git
+   
+    code-server --install-extension ms-python.python 
+
+    code-server --install-extension ms-toolsai.jupyter
+
+    ln -s /workspace/.local/share/code-server/extensions /workspace/extensions
+
+
+    mkdir -p /workspace/User/
+
+
+    echo '{"workbench.colorTheme": "Visual Studio Dark"}' > /workspace/User/settings.json
+
+    python -m venv /workspace/.venv
+
+    source /workspace/.venv/bin/activate
+    
+    /workspace/.venv/bin/python -m pip install --no-cache-dir stactools ipykernel requests
+
+    /workspace/.venv/bin/python -m ipykernel install --user --name zoo_env --display-name "Python (ZOO-Project)"
+
+    export AWS_DEFAULT_REGION="us-east-1"
+
+    export AWS_ACCESS_KEY_ID="test"
+    
+    export AWS_SECRET_ACCESS_KEY="test"
+    
+    aws s3 mb s3://results --endpoint-url=http://eoap-zoo-project-localstack:4566
